05719be000a37d09758ae129258eee98c0103f46
ethanbokame <ethaniraqui@gmail.com>
2026-04-23 15:54:41 +0000

    add minitab based on url

--- app/components/NavLink.vue
+++ app/components/NavLink.vue
@@ -8,14 +8,13 @@ export default {
       required: true
     }
   },
-  data() {
-    return {
-      hover: false
-    }
-  },
-  methods: {
-    toggleHover() {
-      return this.hover = !this.hover
+  computed: {
+    route() {
+      return useRoute()
+    },
+    
+    onThisPage(): boolean {
+      return this.route.path === this.link.to
     }
   }
 }
@@ -23,10 +22,17 @@ export default {
 
 <template>
   <NuxtLink :to="link.to" class="relative group">
+    
     <div class="flex items-center gap-2 py-3 px-8 text-white font-semibold">
       <Icon :name="link.icon" class="text-yellow-300" />
       {{ link.label }}
     </div>
-    <div class="absolute bottom-0 h-[4px] w-0 bg-yellow-300 transition-all duration-300 group-hover:w-full"></div>
+    
+    <div :class="{
+      'absolute bottom-0 h-[4px] w-full bg-yellow-300': onThisPage,
+      'absolute bottom-0 h-[4px] w-0 bg-yellow-300 transition-all duration-300 group-hover:w-full': !onThisPage,
+    }">
+    </div>
+    
   </NuxtLink>
 </template>
