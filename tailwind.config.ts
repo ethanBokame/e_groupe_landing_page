28b9b709bd1e0173439d23f77145a37b8f77e6fd
ethanbokame <ethaniraqui@gmail.com>
2026-04-23 21:35:15 +0000

    add custom font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   theme: {
     extend: {
-      colors: {
-        
+      fontFamily: {
+        custom: ['Inter', 'sans-serif'],
       }
     }
   }
