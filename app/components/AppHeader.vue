5c59c0dddb79ede246a01a701f4406f479da69f5
ethanbokame <ethaniraqui@gmail.com>
2026-04-24 12:58:35 +0000

    add pointer-event property on shadowScreen component

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -85,8 +85,10 @@ export default {
 
     <!-- Shadow screen -->
     <ShadowScreen @click="toggleMenu" :class="[
-      'transition-all duration-200',
-      IsOpen ? 'opacity-100' : 'opacity-0'
+      'transition-opacity duration-300',
+      IsOpen
+        ? 'opacity-100 pointer-events-auto'
+        : 'opacity-0 pointer-events-none'
     ]" />
 
     <!-- mobile header -->
@@ -101,9 +103,9 @@ export default {
       <Icon name="lucide:x" class="h-8 w-8 text-yellow-500 self-end" @click="toggleMenu" />
 
       <nav class="flex flex-col gap-4 p-2 text-white">
-        
+
         <NavLinkMobile v-for="l in links.slice(0, -1)" :key="l.label" :link="l" />
-        
+
         <NuxtLink to="/login" class="flex items-center gap-3 bg-yellow-500 p-2 rounded-xl justify-center">
           <Icon name="solar:user-bold" class="h-5 w-5" />
           Connexion
@@ -113,11 +115,11 @@ export default {
 
     <!-- desktop header -->
     <div class="hidden lg:flex gap-6 items-center">
-      
+
       <InfoSup v-for="i in infos" :key="i.to" :info="i" />
-      
+
       <hr class="h-5 w-[2px] bg-yellow-500 rounded-md">
-      
+
       <div class="flex gap-5 mt-1">
         <SocialNetworkIcon v-for="s in socialsNetworks" :key="s.to" :sN="s" />
       </div>
