da4e9563a246742cad5f40992413e6ce9f6b568d
ethanbokame <ethaniraqui@gmail.com>
2026-05-24 22:01:15 +0000

    add inDeveloppement component and update links data

--- app/components/InDevelopment.vue
+++ app/components/InDevelopment.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="h-80 w-full text-xl bg-gray-100 flex items-center justify-center">En cours de developpement👷‍♂️</div>
+  <div 
+    class="h-80 w-full text-xl bg-gray-100 flex items-center justify-center">
+    En cours de developpement👷‍♂️</div>
 </template>
