d00040397bd853fc884dfb8187f9c8c9cead5f0e
ethanbokame <ethaniraqui@gmail.com>
2026-05-26 13:31:47 +0000

    update head function in googleMap component

--- app/components/GoogleMap.vue
+++ app/components/GoogleMap.vue
@@ -1,18 +1,18 @@
 <script lang="ts">
 export default {
-  props: {
-    link: {
-      type: String,
-      required: true
-    }
-  },
-  useHead() {
-    return {
+  setup() {
+    useHead({
       link: [
         { rel: 'icon', type: 'image/png', href: '/logo.png' },
         { rel: 'dns-prefetch', href: '//www.google.com' },
         { rel: 'preconnect', href: 'https://www.google.com', crossorigin: '' }      
       ]
+    })
+  },
+  props: {
+    link: {
+      type: String,
+      required: true
     }
   }
 }
