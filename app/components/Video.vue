93615dffbf703c1210b916d4ba85530ddf57acc6
ethanbokame <ethaniraqui@gmail.com>
2026-05-26 14:35:39 +0000

    Active loop on the video component

--- app/components/Video.vue
+++ app/components/Video.vue
@@ -20,7 +20,7 @@ export default {
     data-aos="fade-right">
     
     <iframe class="h-[400px] w-full relative z-10"
-      src="https://player.vimeo.com/video/1189304717?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;muted=1&amp;loop=0"
+      src="https://player.vimeo.com/video/1189304717?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;muted=1&amp;loop=1"
       allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
       referrerpolicy="strict-origin-when-cross-origin" title="E-groupe">
     </iframe>
